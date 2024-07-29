6f39e5c62bacfa441f34e039fe81a6f51438987b
JOON-KR <yyj4147@gmail.com>
2024-07-29 14:04:45 +0900

    fix: service 오류수정

--- backend/src/main/java/com/ziguonnana/ziguserver/domain/member/service/MemberService.java
+++ backend/src/main/java/com/ziguonnana/ziguserver/domain/member/service/MemberService.java
@@ -32,71 +32,34 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional
 public class MemberService {
 
-	private final JwtUtil jwtUtil;
-	private final MemberRepository memberRepository;
-	private final PasswordEncoder encoder;
-	private final RedisService redisService;
+    private final JwtUtil jwtUtil;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder encoder;
+    private final RedisService redisService;
 
-	public String login(LoginRequest request) {
-		String email = request.email();
-		String password = request.password();
-		log.info("request.password {}", password);
-		Optional<Member> member = memberRepository.findByEmail(email);
-		if (member.isEmpty()) {
-			throw new UsernameNotFoundException("이메일이 존재하지 않습니다.");
-		}
-		log.info("member.get().getPassword {}", member.get().getPassword());
-		if (!encoder.matches(password, member.get().getPassword())) {
-			throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
-		}
-		CustomUserInfo info = CustomUserInfo.from(member.get());
-		log.info("CustomUserInfoDto {}", info.toString());
-		String accessToken = jwtUtil.createAccessToken(info);
-		String refreshToken = jwtUtil.createRefreshToken(info);
+    public String login(LoginRequest request) {
+        String email = request.email();
+        String password = request.password();
+        log.info("request.password {}", password);
 
-		redisService.saveRefreshToken(info.email(), refreshToken);
-		return accessToken;
-	}
+        Member member = memberRepository.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("이메일이 존재하지 않습니다."));
 
-	public void logout(String email, String refreshToken) {
-		// 리프레시 토큰을 블랙리스트에 추가
-		long remainingTime = jwtUtil.getRemainingTime(refreshToken);
-		redisService.addTokenToBlacklist(refreshToken, remainingTime);
-		redisService.deleteRefreshToken(email);
-	}
+        log.info("member.getPassword {}", member.getPassword());
 
+        if (!encoder.matches(password, member.getPassword())) {
+            throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
+        }
 
-	public Long signup(Member member) {
-		Optional<Member> validMember = memberRepository.findByEmail(member.getEmail());
+        CustomUserInfo info = CustomUserInfo.from(member);
+        log.info("CustomUserInfoDto {}", info.toString());
 
-		if (validMember.isPresent()) {
-			throw new ValidateMemberException("this member email is already exist. " + member.getEmail());
-		}
-		log.info("member.getPassword {}", member.getPassword());
-		member.updatePassword(encoder.encode(member.getPassword()));
-		log.info("member info {}", member);
-		memberRepository.save(member);
-		return member.getId();
-	}
+        String accessToken = jwtUtil.createAccessToken(info);
+        String refreshToken = jwtUtil.createRefreshToken(info);
 
-	public Member update(UpdateRequest request) {
-		Long memberId = TokenInfo.getMemberId();
-		Optional<Member> originMember = memberRepository.findById(memberId);
-		if(originMember.isEmpty()) {
-			throw new MemberNotFoundException();
-		}
-		Member origin = originMember.get();
-		String newPassword = request.password() != null ? encoder.encode(request.password()) : origin.getPassword();
-		String newName = request.name() != null ? request.name() : origin.getName();
-		Member updateMember = Member.builder()
-			.id(memberId)
-			.email(origin.getEmail())
-			.password(newPassword)
-			.name(newName)
-			.role(origin.getRole())
-			.build();
-		return memberRepository.save(updateMember);
-	}
+        redisService.saveRefreshToken(info.email(), refreshToken);
+        return accessToken;
+    }
 
 	public void delete() {
 	    Long memberId = TokenInfo.getMemberId();
@@ -124,31 +87,73 @@ public class MemberService {
 	            .regDate(member.getRegDate())
 	            .build();
 	}
+    public void logout(String email, String refreshToken) {
+        long remainingTime = jwtUtil.getRemainingTime(refreshToken);
+        redisService.addTokenToBlacklist(refreshToken, remainingTime);
+        redisService.deleteRefreshToken(email);
+    }
 
-	public Optional<Member> getMember(Long memberId) {
-		return Optional.ofNullable(memberRepository.findById(memberId)
-			.orElseThrow(() -> new MemberNotFoundException("멤버가 존재하지 않습니다.")));
-	}
+    public Long signup(Member member) {
+        memberRepository.findByEmail(member.getEmail())
+            .ifPresent(validMember -> {
+                throw new ValidateMemberException("this member email is already exist. " + member.getEmail());
+            });
 
-	public Optional<Member> getMemberByEmail(String email) {
-		return memberRepository.findByEmail(email);
-	}
+        log.info("member.getPassword {}", member.getPassword());
+        member.updatePassword(encoder.encode(member.getPassword()));
+        log.info("member info {}", member);
+        memberRepository.save(member);
+        return member.getId();
+    }
 
-	public EmailListResponse getEmails() {
-		List<String> emails = memberRepository.findAll().stream()
-			.map(Member::getEmail)
-			.toList();
+    public Member update(UpdateRequest request) {
+        Long memberId = TokenInfo.getMemberId();
 
-		return EmailListResponse.builder().emails(emails).build();
-	}
+        Member origin = memberRepository.findById(memberId)
+            .orElseThrow(MemberNotFoundException::new);
 
-	public CustomUserInfo getUserInfo(String email) {
-		Member member = memberRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-		return CustomUserInfo.from(member);
-	}
+        String newPassword = Optional.ofNullable(request.password())
+            .map(encoder::encode)
+            .orElse(origin.getPassword());
 
+        String newName = Optional.ofNullable(request.name())
+            .orElse(origin.getName());
 
-	public void sameUserCheck(Long memberId, Long othersMemberId) {
-		if (memberId.equals(othersMemberId)) throw new SameMemberException();
-	}
+        Member updateMember = Member.builder()
+            .id(memberId)
+            .email(origin.getEmail())
+            .password(newPassword)
+            .name(newName)
+            .role(origin.getRole())
+            .build();
+
+        return memberRepository.save(updateMember);
+    }
+
+    public Optional<Member> getMember(Long memberId) {
+        return Optional.ofNullable(memberRepository.findById(memberId)
+            .orElseThrow(() -> new MemberNotFoundException("멤버가 존재하지 않습니다.")));
+    }
+
+    public Optional<Member> getMemberByEmail(String email) {
+        return memberRepository.findByEmail(email);
+    }
+
+    public EmailListResponse getEmails() {
+        List<String> emails = memberRepository.findAll().stream()
+            .map(Member::getEmail)
+            .toList();
+
+        return EmailListResponse.builder().emails(emails).build();
+    }
+
+    public CustomUserInfo getUserInfo(String email) {
+        Member member = memberRepository.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return CustomUserInfo.from(member);
+    }
+
+    public void sameUserCheck(Long memberId, Long othersMemberId) {
+        if (memberId.equals(othersMemberId)) throw new SameMemberException();
+    }
 }
