79cfb1befd0578278712222acbaacc9cd5774093
gawon <gawon_127@naver.com>
2024-08-02 13:39:40 +0900

    feat:introduction-guide

--- frontend/src/pages/games/Game1.jsx
+++ frontend/src/pages/games/Game1.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import GameInfoModal from "../../components/modals/GameInfoModal";
 import blue from "../../assets/icons/blue.png";
 import styled from "styled-components";
+import IntroductionGuideModal from "../../components/modals/IntroductionGuideModal";
+import IntroductionModat from "../../components/modals/IntroductionModal";
+// 자기소개 문답 페이지
 
 const Wrap = styled.div`
   display: flex;
@@ -13,13 +16,25 @@ const Wrap = styled.div`
 `;
 
 const Game1 = () => {
-  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [isIntroGuideModalOpen, setIsIntroGuideModalOpen] = useState(true);
 
-  const onClose = () => {
-    setIsModalOpen(false);
+
+  const closeIntroGuideModal = () => {
+    setIsIntroGuideModalOpen(false);
   };
 
-  return <Wrap>Game1</Wrap>;
+  return (
+    <Wrap>
+      <div>
+        Game1
+      </div>
+      {/* {isIntroGuideModalOpen && (
+        <IntroductionGuideModal onClose={closeIntroGuideModal} />
+      )} */}
+
+
+    </Wrap>
+  );
 };
 
 export default Game1;
