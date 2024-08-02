91b3ed251d5ba00a5398b3efc8f2860fb615f9a0
hj-k66 <hjx66@naver.com>
2024-08-02 13:41:46 +0900

    fix: RedisConfig application.yml 설정으로 변경

--- backend/src/main/java/com/ziguonnana/ziguserver/configuration/RedisConfig.java
+++ backend/src/main/java/com/ziguonnana/ziguserver/configuration/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.ziguonnana.ziguserver.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,6 +10,12 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
