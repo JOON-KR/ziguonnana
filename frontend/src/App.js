da66fa71bc159a765e33acc8830559ed97765c1c
nwy98 <97599088+norunaru@users.noreply.github.com>
2024-08-04 15:35:01 +0900

    fix: context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,25 +11,32 @@ import Intro from "./pages/iceBreaking/Intro";
 import GameRecord from "./pages/games/GameRecord";
 import RoomCreateModal from "./components/modals/RoomCreateModal";
 import { WebSocketProvider } from "./context/WebSocketContext";
+import { AppProvider } from "./context/AppContext"; // 추가된 부분
 
 function App() {
   return (
-    <WebSocketProvider>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/gameRecord" element={<GameRecord />} />
-          <Route path="/user/*" element={<UserPages />} />
-          <Route path="/icebreaking" element={<IceBreaking />}>
-            <Route path="/icebreaking/games/*" element={<GamePages />} />
-            <Route path="" element={<Loading />} />
-            <Route path="intro" element={<Intro />} />
-          </Route>
-          <Route path="/create-room" element={<RoomCreateModal />} />
-        </Routes>
-      </div>
-    </WebSocketProvider>
+    <AppProvider>
+      {" "}
+      {/* AppProvider로 전체를 감싸줍니다. */}
+      <WebSocketProvider>
+        {" "}
+        {/* WebSocketProvider도 함께 사용합니다. */}
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/gameRecord" element={<GameRecord />} />
+            <Route path="/user/*" element={<UserPages />} />
+            <Route path="/icebreaking" element={<IceBreaking />}>
+              <Route path="/icebreaking/games/*" element={<GamePages />} />
+              <Route path="" element={<Loading />} />
+              <Route path="intro" element={<Intro />} />
+            </Route>
+            <Route path="/create-room" element={<RoomCreateModal />} />
+          </Routes>
+        </div>
+      </WebSocketProvider>
+    </AppProvider>
   );
 }
 
