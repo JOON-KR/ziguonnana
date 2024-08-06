4b5c6ad747e98dcf0af6994b0594ee9d4fb5424e
노우영 <nwy98@naver.com>
2024-08-06 10:58:36 +0900

    .

--- frontend/src/components/modals/RoomJoinModal.jsx
+++ frontend/src/components/modals/RoomJoinModal.jsx
@@ -167,12 +167,11 @@ const RoomJoinModal = ({ onClose }) => {
       });
 
       console.log("오픈비두 엔드포인트 연결 : ", response);
-      const openviduToken = response.data.data.openviduToken; // 수정된 부분: 응답에서 openviduToken 추출
-      console.log("OpenVidu Token:", openviduToken);
 
       dispatch(setRoomId(inviteCode));
+      console.log("Invite Code:", inviteCode);
 
-      // 소켓 생성
+      //소켓 생성
       const socket = new SockJS(`${BASE_URL}/ws`);
       console.log("Socket created");
 
@@ -188,7 +187,7 @@ const RoomJoinModal = ({ onClose }) => {
         const serverUrl = socket._transport.url;
         console.log("서버 연결됨 : ", serverUrl);
 
-        // roomId를 소켓 엔드포인트로 연결하면서 보냄
+        //roomId를 소켓 엔드포인트로 연결하면서 보냄
         stompClient.subscribe(`/game/${inviteCode}/join`, (message) => {
           console.log(
             `/game/${inviteCode}/join 에서 온 메세지 : `,
@@ -202,19 +201,27 @@ const RoomJoinModal = ({ onClose }) => {
         stompClient.activate();
         console.log("STOMP Client activated");
 
-      if (stClient && stClient.connected) {
-        stClient.send(`/app/game/${inviteCode}/${memId}/join`);
-      }
+        if (stClient && stClient.connected) {
+          stClient.send(`/app/game/${inviteCode}/${memId}/join`);
+        }
 
-      // 로그인 여부를 state에 포함시켜 navigate
-      // navigate("/user/profilePick", {
-      //   state: {
-      //     inviteCode,
-      //     isJoin: true,
-      //     isLoggedIn: isLoggedIn,
-      //     from: "joinModal",
-      //   },
-      // });
+        // 로그인 여부를 state에 포함시켜 navigate
+        navigate("/user/profilePick", {
+          state: {
+            inviteCode,
+            isJoin: true,
+            isLoggedIn: isLoggedIn,
+            from: "joinModal",
+          },
+        });
+      };
+
+      stompClient.onStompError = (frame) => {
+        console.error("Broker reported error: " + frame.headers["message"]);
+        console.error("Additional details: " + frame.body);
+      };
+
+      stompClient.activate();
     } catch (e) {
       console.error("방참여 오류", e);
     }
