118b39ac98a976d197b258fce50e804143c3d427
gitlab 이름 <nwy98@naver.com>
2024-08-07 02:09:36 +0900

    feat: 게임 선택, 몸으로 말해요

--- frontend/src/pages/games/Game1.jsx
+++ frontend/src/pages/games/Game1.jsx
@@ -6,6 +6,7 @@ import IntroductionGuideModal from "../../components/modals/IntroductionGuideMod
 import IntroductionModal from "../../components/modals/IntroductionModal";
 import blue from "../../assets/icons/blue.png";
 import { ReactSketchCanvas } from "react-sketch-canvas";
+import { useSelector } from "react-redux";
 
 const Wrap = styled.div`
   width: 100%;
@@ -135,22 +136,26 @@ const ColorSquare = styled.div`
   border: ${(props) => (props.selected ? "2px solid #000" : "none")};
 `;
 
-const Game1 = ({ roomId }) => {
+const Game1 = () => {
   const [isIntroGuideModalOpen, setIsIntroGuideModalOpen] = useState(true); // IntroductionWelcomeModal 상태
   const [isIntroModalOpen, setIsIntroModalOpen] = useState(false); // IntroductionModal 상태
-  const [isDrawingWelcomeModalOpen, setIsDrawingWelcomeModalOpen] = useState(false); // DrawingWelcomeModal 상태
+  const [isDrawingWelcomeModalOpen, setIsDrawingWelcomeModalOpen] =
+    useState(false); // DrawingWelcomeModal 상태
   const [isDrawingGuideModalOpen, setIsDrawingGuideModalOpen] = useState(false); // DrawingGuideModal 상태
   const [brushColor, setBrushColor] = useState("#000000"); // 브러시 색상 상태
   const [brushRadius, setBrushRadius] = useState(5); // 브러시 크기 상태
   const [isEraser, setIsEraser] = useState(false);
   const [timeLeft, setTimeLeft] = useState(20);
-  const [memberId, setMemberId] = useState(''); // 현재 사용자 ID 상태
+  const [memberId, setMemberId] = useState(""); // 현재 사용자 ID 상태
   const [error, setError] = useState(""); // 에러 메시지 상태
   const [drawingHistory, setDrawingHistory] = useState([]); // 그린 과정 저장
   const [isReplaying, setIsReplaying] = useState(false);
   const [replayIndex, setReplayIndex] = useState(0);
   const canvasRef = useRef(null);
   const animationFrameRef = useRef(null);
+
+  const roomId = useSelector((state) => state.room.roomId);
+  const client = useSelector((state) => state.client.stompClient);
 
   const openIntroModal = () => {
     setIsIntroGuideModalOpen(false);
@@ -205,13 +210,20 @@ const Game1 = ({ roomId }) => {
   };
 
   const formatTime = (time) => {
-    const minutes = Math.floor(time / 60).toString().padStart(2, '0');
-    const seconds = (time % 60).toString().padStart(2, '0');
+    const minutes = Math.floor(time / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (time % 60).toString().padStart(2, "0");
     return `${minutes}:${seconds}`;
   };
 
   useEffect(() => {
-    if (!isIntroGuideModalOpen && !isIntroModalOpen && !isDrawingWelcomeModalOpen && !isDrawingGuideModalOpen) {
+    if (
+      !isIntroGuideModalOpen &&
+      !isIntroModalOpen &&
+      !isDrawingWelcomeModalOpen &&
+      !isDrawingGuideModalOpen
+    ) {
       if (timeLeft > 0) {
         const timer = setTimeout(() => {
           setTimeLeft(timeLeft - 1);
@@ -222,7 +234,24 @@ const Game1 = ({ roomId }) => {
         startReplay();
       }
     }
-  }, [timeLeft, isIntroGuideModalOpen, isIntroModalOpen, isDrawingWelcomeModalOpen, isDrawingGuideModalOpen]);
+  }, [
+    timeLeft,
+    isIntroGuideModalOpen,
+    isIntroModalOpen,
+    isDrawingWelcomeModalOpen,
+    isDrawingGuideModalOpen,
+  ]);
+
+  useEffect(() => {
+    console.log("--------------------------------");
+    console.log("연결 상태 : ", client.connected);
+    console.log("--------------------------------");
+
+    client.send(`/topic/game/${roomId}`, (message) => {
+      const parsedMessage = JSON.parse(message.body);
+      console.log("받은 메시지 : ", parsedMessage);
+    });
+  }, []);
 
   const replayDrawing = () => {
     if (replayIndex < drawingHistory.length) {
@@ -241,24 +270,47 @@ const Game1 = ({ roomId }) => {
   }, [isReplaying, replayIndex, drawingHistory]);
 
   const colors = [
-    "#FF0000", "#FF7F00", "#FFFF00", "#00FF00", "#0000FF", "#4B0082", "#8B00FF", "#000000", "#FFFFFF",
-    "#A52A2A", "#D2691E", "#DAA520", "#808000", "#008000", "#008080", "#00FFFF", "#4682B4", "#00008B",
-    "#8A2BE2", "#FF1493", "#D3D3D3", "#A9A9A9"
+    "#FF0000",
+    "#FF7F00",
+    "#FFFF00",
+    "#00FF00",
+    "#0000FF",
+    "#4B0082",
+    "#8B00FF",
+    "#000000",
+    "#FFFFFF",
+    "#A52A2A",
+    "#D2691E",
+    "#DAA520",
+    "#808000",
+    "#008000",
+    "#008080",
+    "#00FFFF",
+    "#4682B4",
+    "#00008B",
+    "#8A2BE2",
+    "#FF1493",
+    "#D3D3D3",
+    "#A9A9A9",
   ];
 
   return (
     <Wrap>
-      {error && <div style={{ color: "red" }}>{error}</div>} {/* 에러 메시지 표시 */}
+      {error && <div style={{ color: "red" }}>{error}</div>}{" "}
+      {/* 에러 메시지 표시 */}
       {isIntroGuideModalOpen && (
-        <IntroductionGuideModal onClose={closeIntroGuideModal} onConfirm={openIntroModal} />
+        <IntroductionGuideModal
+          onClose={closeIntroGuideModal}
+          onConfirm={openIntroModal}
+        />
       )}
       {isIntroModalOpen && (
         <IntroductionModal onClose={openDrawingWelcomeModal} />
       )}
       {isDrawingWelcomeModalOpen && (
-        <GameInfoModal 
+        <GameInfoModal
           planetImg={blue}
-          RedBtnText={"게임 시작"}     
+          RedBtnText={"게임 시작"}
           RedBtnFn={closeDrawingWelcomeModal}
           BlueBtnText={"게임 설명"}
           BlueBtnFn={openDrawingGuideModal}
@@ -268,70 +320,82 @@ const Game1 = ({ roomId }) => {
       )}
       {isDrawingGuideModalOpen && (
         <GameModal
-          RedBtnText={"게임 시작"}    
+          RedBtnText={"게임 시작"}
           RedBtnFn={closeDrawingGuideModal}
-          modalText={(
+          modalText={
             <>
-              주어지는 이미지와 특징을 바탕으로 <br /> 아바타를 그려주세요. <br />
+              주어지는 이미지와 특징을 바탕으로 <br /> 아바타를 그려주세요.{" "}
+              <br />
               제한시간은 20초입니다.
             </>
-          )}
+          }
           onClose={closeDrawingGuideModal}
         />
       )}
-      {!isIntroGuideModalOpen && !isIntroModalOpen && !isDrawingWelcomeModalOpen && !isDrawingGuideModalOpen && (
-        <>
-          <Header>
-            <ProfileInfo>
-              <ProfileImage src="path/to/profile-image.png" alt="프로필 이미지" />
-              <ProfileDetails>
-                <HeaderText>이름: 홍길동</HeaderText>
-                <HeaderText>키워드: #뾰족코 #근엄한</HeaderText>
-              </ProfileDetails>
-            </ProfileInfo>
-            <HeaderText>주어진 정보를 활용하여 아바타를 그려주세요!</HeaderText>
-          </Header>
-          <CanvasWrapper>
-            <ReactSketchCanvas
-              ref={canvasRef}
-              width="970px"
-              height="600px"
-              strokeColor={isEraser ? "#FFFFFF" : brushColor}
-              strokeWidth={brushRadius}
-              eraserWidth={isEraser ? brushRadius : 0}
-            />
-            <ToolsWrapper>
-              <CustomSwatchesPicker>
-                {colors.map((color) => (
-                  <ColorSquare
-                    key={color}
-                    color={color}
-                    selected={brushColor === color}
-                    onClick={() => handleColorChange(color)}
-                  />
-                ))}
-              </CustomSwatchesPicker>
-              <SliderWrapper>
-                <SliderLabel>펜 굵기</SliderLabel>
-                <Slider
-                  type="range"
-                  min="1"
-                  max="20"
-                  value={brushRadius}
-                  onChange={(e) => setBrushRadius(e.target.value)}
+      {!isIntroGuideModalOpen &&
+        !isIntroModalOpen &&
+        !isDrawingWelcomeModalOpen &&
+        !isDrawingGuideModalOpen && (
+          <>
+            <Header>
+              <ProfileInfo>
+                <ProfileImage
+                  src="path/to/profile-image.png"
+                  alt="프로필 이미지"
                 />
-              </SliderWrapper>
-              <ToolButton onClick={() => setIsEraser(false)} active={!isEraser}>
-                펜
-              </ToolButton>
-              <ToolButton onClick={() => setIsEraser(true)} active={isEraser}>
-                지우개
-              </ToolButton>
-              <Timer>{formatTime(timeLeft)}</Timer>
-            </ToolsWrapper>
-          </CanvasWrapper>
-        </>
-      )}
+                <ProfileDetails>
+                  <HeaderText>이름: 홍길동</HeaderText>
+                  <HeaderText>키워드: #뾰족코 #근엄한</HeaderText>
+                </ProfileDetails>
+              </ProfileInfo>
+              <HeaderText>
+                주어진 정보를 활용하여 아바타를 그려주세요!
+              </HeaderText>
+            </Header>
+            <CanvasWrapper>
+              <ReactSketchCanvas
+                ref={canvasRef}
+                width="970px"
+                height="600px"
+                strokeColor={isEraser ? "#FFFFFF" : brushColor}
+                strokeWidth={brushRadius}
+                eraserWidth={isEraser ? brushRadius : 0}
+              />
+              <ToolsWrapper>
+                <CustomSwatchesPicker>
+                  {colors.map((color) => (
+                    <ColorSquare
+                      key={color}
+                      color={color}
+                      selected={brushColor === color}
+                      onClick={() => handleColorChange(color)}
+                    />
+                  ))}
+                </CustomSwatchesPicker>
+                <SliderWrapper>
+                  <SliderLabel>펜 굵기</SliderLabel>
+                  <Slider
+                    type="range"
+                    min="1"
+                    max="20"
+                    value={brushRadius}
+                    onChange={(e) => setBrushRadius(e.target.value)}
+                  />
+                </SliderWrapper>
+                <ToolButton
+                  onClick={() => setIsEraser(false)}
+                  active={!isEraser}
+                >
+                  펜
+                </ToolButton>
+                <ToolButton onClick={() => setIsEraser(true)} active={isEraser}>
+                  지우개
+                </ToolButton>
+                <Timer>{formatTime(timeLeft)}</Timer>
+              </ToolsWrapper>
+            </CanvasWrapper>
+          </>
+        )}
     </Wrap>
   );
 };
--- frontend/src/pages/games/Game2.jsx
+++ frontend/src/pages/games/Game2.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GameModal from "../../components/modals/GameModal";
 import GameInfoModal from "../../components/modals/GameInfoModal";
 import styled from "styled-components";
 import orange from "../../assets/icons/orange.png";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Wrap = styled.div`
   width: 90%;
@@ -15,18 +17,111 @@ const Wrap = styled.div`
 
 // 몸으로 말해요 페이지 (BodyTalk)
 const Game2 = () => {
-  const [isBodyTalkWelcomeModalOpen, setIsBodyTalkWelcomeModalOpen] = useState(true);
-  const [isBodyTalkGuideModalOpen, setIsBodyTalkGuideModalOpen] = useState(false)
+  const [isBodyTalkWelcomeModalOpen, setIsBodyTalkWelcomeModalOpen] =
+    useState(true);
+  const [isBodyTalkGuideModalOpen, setIsBodyTalkGuideModalOpen] =
+    useState(false);
+  const roomId = useSelector((state) => state.room.roomId);
+  const client = useSelector((state) => state.client.stompClient);
+  const navigate = useNavigate();
+  const userNo = useSelector((state) => state.auth.userNo);
+  const maxNo = useSelector((state) => state.room.maxNo);
+
+  const [round, setRound] = useState(1);
+  const [keywordType, setKeywordType] = useState(""); //제시어의 분류 : 동물, 악기 등등
+  const [receivedKeyword, setReceivedKeyword] = useState("");
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [durationTime, setDurationTime] = useState(0);
 
   // isBodyTalkWelcomeModalOpen 닫고 isBodyTalkGuideModalOpen 열기
+
+  //최초 1회 실행
+  useEffect(() => {
+    console.log("--------------------------------");
+    console.log("연결 상태 : ", client.connected);
+    console.log("--------------------------------");
+    if (client && client.connected) {
+      //멤버아이디로 구독 - 몸으로 표현하는사람은 이거를 통해 받음
+      client.subscribe(`/topic/game/${roomId}/${userNo}`, (message) => {
+        const parsedMessage = JSON.parse(message.body);
+        console.log("출제자 키워드 :", parsedMessage.data);
+        setReceivedKeyword(parsedMessage.data.word);
+
+        // {
+        //   "message" : "SUCCESS",
+        //   "data" : {
+        //         "type" : "동물",
+        //         "word" : "악어"
+        //     },
+        //   "commandType" : "EXPLANIER"
+        // }
+
+        // 결과
+        if (
+          parsedMessage.commandType == "RESULT" &&
+          parsedMessage.data.durationTime
+        ) {
+          setDurationTime(parsedMessage.data.durationTime);
+          console.log("진행 시간 : ", parsedMessage.data.durationTime);
+        }
+        // {
+        //   "message" : "SUCCESS",
+        //   "commandType" : "RESULT",
+        //   "data" : {
+        //     "correctCnt" : 5,  // 맞은 갯수
+        //     "durationTime" : 80  // 게임 종료까지 걸린 시간(초)
+        //   }
+        // }
+      });
+
+      //방 구독
+      client.subscribe(`/topic/game/${roomId}`, (message) => {
+        const parsedMessage = JSON.parse(message.body);
+        console.log("키워드 타입 :", parsedMessage.data);
+        setKeywordType(parsedMessage.data);
+        // {
+
+        //   "message" : "SUCCESS",
+        //   "CommandType" : "KEYWORD_TYPE",
+        //   "data" : "동물"
+        // }
+
+        //서버에서 응답 받는데 채팅친게 정답이면 다음 라운드로
+        if (parsedMessage.data.isCorrect == true) {
+          setRound((prevRound) => prevRound + 1);
+        }
+      });
+    }
+  }, []);
+
+  //라운드 변경시 실행
+  useEffect(() => {
+    // 정답을 맞추면 다음 턴으로 이동 ⇒ 키워드 요청 api 호출
+    if (client && client.connected) {
+      client.send(`/app/game/${roomId}/bodyTalk/keyword`);
+    }
+  }, [round]);
+
   const openBodyTalkGuideModal = () => {
     setIsBodyTalkWelcomeModalOpen(false);
     setIsBodyTalkGuideModalOpen(true);
-  }
+  };
 
   const closeBodyTalkGuideModal = () => {
     setIsBodyTalkGuideModalOpen(false);
-  }
+  };
+
+  //이후에 채팅 친 내용 보낼 떄 아래 코드 갖다 사용
+  // if (client && client.connected) {
+  //   client.send(
+  //     `/app/game/${roomId}/bodyTalk/chat`,
+  //     {},
+  //     JSON.stringify({
+  //       senderNum: userNo,
+  //       content: typedChat,
+  //     })
+  //   );
+  // }
 
   return (
     <Wrap>
@@ -34,7 +129,7 @@ const Game2 = () => {
         <GameInfoModal
           planetImg={orange}
           RedBtnText={"게임 시작"}
-          // {/* 게임시작 버튼 누르면 모달 닫고 페이지에서 진행 */}      
+          // {/* 게임시작 버튼 누르면 모달 닫고 페이지에서 진행 */}
           RedBtnFn={closeBodyTalkGuideModal}
           BlueBtnText={"게임 설명"}
           BlueBtnFn={openBodyTalkGuideModal}
@@ -45,20 +140,27 @@ const Game2 = () => {
       {isBodyTalkGuideModalOpen && (
         <GameModal
           RedBtnText={"게임 시작"}
-          // {/* 게임시작 버튼 누르면 모달 닫고 페이지에서 진행 */}      
-          RedBtnFn={closeBodyTalkGuideModal}
-          modalText={(
+          // {/* 게임시작 버튼 누르면 모달 닫고 페이지에서 진행 */}
+          RedBtnFn={() => {
+            closeBodyTalkGuideModal();
+            if (client && client.connected) {
+              client.send(`/app/game/${roomId}/bodyTalk/keyword`);
+              setIsGameStarted(true);
+            }
+          }}
+          modalText={
             <>
-            한 명은 제시어를 몸으로 표현하고, <br /> 나머지는 제시어를 맞추면 됩니다. <br />
-            제한 시간은 4분입니다.
+              한 명은 제시어를 몸으로 표현하고, <br /> 나머지는 제시어를 맞추면
+              됩니다. <br />
+              제한 시간은 4분입니다.
             </>
-          )}
-          onClose={() => setIsBodyTalkGuideModalOpen(false)}
+          }
+          onClose={() => {
+            setIsBodyTalkGuideModalOpen(false);
+          }}
         />
       )}
-      
       몸으로 말해요 게임 화면
-
     </Wrap>
   );
 };
