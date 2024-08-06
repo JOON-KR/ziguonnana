93af7c5accbe733be59b70a1a341c535b3d578f4
gawon <gawon_127@naver.com>
2024-08-06 16:57:17 +0900

    feat:create-challenge-pages

---
+++ frontend/src/pages/games/Game5Dance.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Wrap = styled.div`
+  width: 90%;
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+// 댄스 챌린지 페이지
+const Game5Dance = () => {
+
+  return (
+    <Wrap>
+      <div>댄스 챌린지 화면</div>
+    </Wrap>
+  );
+};
+
+export default Game5Dance;
