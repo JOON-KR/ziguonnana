cbf0843bc81d545bab45c6b07a67f03c2fa92ec7
JOON-KR <yyj4147@gmail.com>
2024-08-07 09:54:24 +0900

    fix: 이구동성dto생성자추가

--- backend/src/main/java/com/ziguonnana/ziguserver/websocket/igudongseong/dto/IgudongseongResult.java
+++ backend/src/main/java/com/ziguonnana/ziguserver/websocket/igudongseong/dto/IgudongseongResult.java
@@ -1,10 +1,14 @@
 package com.ziguonnana.ziguserver.websocket.igudongseong.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class IgudongseongResult {
 	int Success;
 	String keyword;
