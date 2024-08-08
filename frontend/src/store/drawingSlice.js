4b1415d6b09d0a78dcd4ed5623ee06f5bd24788e
gawon <gawon_127@naver.com>
2024-08-08 10:12:11 +0900

    fix:connecetDrawing

--- frontend/src/store/drawingSlice.js
+++ frontend/src/store/drawingSlice.js
@@ -3,26 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const drawingSlice = createSlice({
   name: "drawing",
   initialState: {
-    drawingData: {
-      "message": "그림 전파",
-      "data": {
-        "1": {
-          "num": 1,
-          "keyword": "예시 키워드1",
-          "art": [10, 20, 30, 40]  // Byte 배열의 예시
-        },
-        "2": {
-          "num": 2,
-          "keyword": "예시 키워드2",
-          "art": [50, 60, 70, 80]  // Byte 배열의 예시
-        },
-        "3": {
-          "num": 3,
-          "keyword": "예시 키워드3",
-          "art": [90, 100, 110, 120]  // Byte 배열의 예시
-        }
-      }
-    }
+    drawingData: {}
   },
   reducers: {
     setDrawingData: (state, action) => {
@@ -33,3 +14,22 @@ export const drawingSlice = createSlice({
 
 export const { setDrawingData } = drawingSlice.actions;
 export default drawingSlice;
+
+// "message": "그림 전파",
+// "data": {
+//   "1": {
+//     "num": 1,
+//     "keyword": "예시 키워드1",
+//     "art": [10, 20, 30, 40]  // Byte 배열의 예시
+//   },
+//   "2": {
+//     "num": 2,
+//     "keyword": "예시 키워드2",
+//     "art": [50, 60, 70, 80]  // Byte 배열의 예시
+//   },
+//   "3": {
+//     "num": 3,
+//     "keyword": "예시 키워드3",
+//     "art": [90, 100, 110, 120]  // Byte 배열의 예시
+//   }
+// }
