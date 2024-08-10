a7a0dd5691f9a31ea2a92a73d92051a334f6026a
nwy98 <97599088+norunaru@users.noreply.github.com>
2024-08-10 17:17:32 +0900

    .

--- frontend/src/api/APIconfig.js
+++ frontend/src/api/APIconfig.js
@@ -1,4 +1,4 @@
 const BASE_URL = "https://i11b303.p.ssafy.io";
-export const TAMTAM_URL = "http://172.30.1.44:8081";
+export const TAMTAM_URL = "http://172.30.1.81:8081";
 
 export default BASE_URL;
