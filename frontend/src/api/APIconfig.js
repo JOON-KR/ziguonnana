ed09e739042754b897b7fc0fea19ffdb569a023a
이정원 <jw17111@naver.com>
2024-08-12 09:38:32 +0900

    Update frontend/src/api/APIconfig.js

--- frontend/src/api/APIconfig.js
+++ frontend/src/api/APIconfig.js
@@ -1,8 +1,4 @@
 const BASE_URL = "https://i11b303.p.ssafy.io";
-<<<<<<< HEAD
-=======
-export const TAMTAM_URL = "http://172.30.1.29:8081";
 export const S3_BASE_URL = "https://ziguonnana.s3.ap-northeast-2.amazonaws.com";
->>>>>>> 1914beb0d2144765d64724cc0fbac89c5ae0d1b7
 
 export default BASE_URL;
